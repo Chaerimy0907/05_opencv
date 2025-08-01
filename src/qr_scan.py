85aecd4371e7dd8ffb1fa7b6e0c1eaa2d940883d
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-01 10:37:52 +0900

    이미지 그레이스케일로 변환

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -3,8 +3,10 @@ import matplotlib.pylab as plt
 import pyzbar.pyzbar as pyzbar
 
 img = cv2.imread('../img/frame.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-plt.imshow(img)
+#plt.imshow(img)
+plt.imshow(gray, cmap='gray')
 plt.show()
 
 cv2.waitKey(0)
