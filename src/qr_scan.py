83af7a661b4a6ae299a2c844e889c73fd45a2afb
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-01 10:38:45 +0900

    주석 추가

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -2,9 +2,13 @@ import cv2
 import matplotlib.pylab as plt
 import pyzbar.pyzbar as pyzbar
 
+# 이미지 불러오기
 img = cv2.imread('../img/frame.png')
+
+# 이미지 그레이 스케일로 변환
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# 결과 출력
 #plt.imshow(img)
 plt.imshow(gray, cmap='gray')
 plt.show()
